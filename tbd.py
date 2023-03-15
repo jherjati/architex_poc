c018437be21b43ef3da4b1862aa5c257b1890faf
Miko <jherjati@gmail.com>
2023-03-15 07:26:50 +0700

    add nginx parse

--- tbd.py
+++ tbd.py
@@ -1,4 +1,6 @@
 import yaml
+import os
+import crossplane
 from yaml.loader import SafeLoader
 from diagrams import Diagram
 from diagrams.c4 import Person, Container, Database, SystemBoundary, Relationship
@@ -28,12 +30,16 @@ def volume2database(volume, access_mode):
 
 
 with open("compose.yaml") as f:
-    data = yaml.load(f, Loader=SafeLoader)
-    with Diagram("Software",  graph_attr=graph_attr, show=True):
+    docker_compose = yaml.load(f, Loader=SafeLoader)
+    nginx_conf = crossplane.parse(
+        f'{os.getcwd()}/nginx.conf')
+    print(nginx_conf.get("config")[0].get("parsed"))
+
+    with Diagram("Software",  graph_attr=graph_attr, show=False):
         containers, databases, networks = {}, {}, {}
 
         # Database initiation
-        for name, service in data["services"].items():
+        for name, service in docker_compose["services"].items():
             if "volumes" in service:
                 for volume_string in service["volumes"]:
                     volume, container_path, access_mode = volume_string.split(
@@ -42,11 +48,11 @@ with open("compose.yaml") as f:
 
         with SystemBoundary("Default Compose Network"):
             # Container initiation
-            for name, service in data["services"].items():
+            for name, service in docker_compose["services"].items():
                 containers[name] = service2container(name, service)
 
         # Relation identification
-        for name, service in data["services"].items():
+        for name, service in docker_compose["services"].items():
             if "depends_on" in service:
                 containers[name] >> Relationship("depends on") >> list(map(
                     lambda n: containers[n], service.get("depends_on")))
