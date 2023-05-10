5f9fd5b339ec2f4ffe668d713a04b611666af574
Miko <jherjati@gmail.com>
2023-05-10 10:59:01 +0700

    typer RP tuts step 4

---
+++ Architex/rptodo.py
@@ -0,0 +1,16 @@
+"""This module provides the RP To-Do model-controller."""
+# rptodo/rptodo.py
+
+from typing import Any, Dict, NamedTuple
+from pathlib import Path
+from architex.database import DatabaseHandler
+
+
+class CurrentTodo(NamedTuple):
+    todo: Dict[str, Any]
+    error: int
+
+
+class Todoer:
+    def __init__(self, db_path: Path) -> None:
+        self._db_handler = DatabaseHandler(db_path)
